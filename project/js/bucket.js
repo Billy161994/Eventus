389c16a275e4a0be9187348ee5f1cf0fe1f031f0
Bilwa Joglekar <bilwa,joglekar@gmail.com>
2018-12-01 10:43:28 -0500

    Three push

--- project/js/bucket.js
+++ project/js/bucket.js
@@ -30,41 +30,41 @@ function move() {
 
 
 
-$(document).ready(function() {
-
-  $("a.btn").click(function(event) {
-    event.preventDefault();
-    $(".jumbotron").toggle('pulsate',1400); // jQuery UI effect 'pulsate'
-    $('h1').text('Whatup!');
-  });
-
-  $('.jumbotron').hide().delay(1000).fadeIn(1400);
-
-  // jQuery UI Widgets
-
-  $( "#tabs" ).tabs();
-  $( "#accordion" ).accordion();
-
-});
-
-
+// $(document).ready(function() {
+//
+//   $("a.btn").click(function(event) {
+//     event.preventDefault();
+//     $(".jumbotron").toggle('pulsate',1400); // jQuery UI effect 'pulsate'
+//     $('h1').text('Whatup!');
+//   });
+//
+//   $('.jumbotron').hide().delay(1000).fadeIn(1400);
+//
+//   // jQuery UI Widgets
+//
+//   $( "#tabs" ).tabs();
+//   $( "#accordion" ).accordion();
+//
+// });
 
 
 
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: {lat: 18.507024, lng: 73.849575}
-  });
 
-  var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-  var beachMarker = new google.maps.Marker({
-    position: {lat: 18.507024, lng: 73.849575},
-    map: map,
-    icon: image
-  });
-}
+
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: {lat: 18.507024, lng: 73.849575}
+//   });
+//
+//   var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+//   var beachMarker = new google.maps.Marker({
+//     position: {lat: 18.507024, lng: 73.849575},
+//     map: map,
+//     icon: image
+//   });
+// }
 
 
 
@@ -76,34 +76,31 @@ google.charts.setOnLoadCallback(drawChart);
 
 
 function drawChart() {
-  var data = new
-google.visualization.DataTable();
+  var data = new google.visualization.DataTable();
   data.addColumn('string', 'Skill');
   data.addColumn('number', 'Points');
   data.addRows([
-    ['HTML', 922],
-    ['CSS', 2679],
-    ['JavaScript', 12899],
-    ['PHP', 1450],
-    ['Development Tools', 1459],
-    ['Java', 275],
-    ['Digital Literacy', 40],
-    ['Databases', 1786],
-    ['Security', 540],
-    ['Quality Assurance', 207]
+    ['Food', 200],
+    ['Lights', 500],
+    ['Furniture', 500],
+    ['Transport', 1450],
+    ['Tools', 500],
+
 ]);
 
 
 // Set chart options
 var options = {
-  'title':'Tony\'s Earned Treehouse Study Units',
-  'width':550,
-  'height':400
+  'title':'Funds',
+  'width':1000,
+  'height':500
 };
 
 
 // Display the chart
-var chart = new
-google.visualization.PieChart(document.getElementById('pieChart'));
-chart.draw(data, options);
+if(window.location.pathname == "/Users/bilwa/Desktop/Eventus/project/charts.html"){
+  var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
+  chart.draw(data, options);
+}
+
 } // Don’t forget to close your function
