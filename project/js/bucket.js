81b76652e967077fc2dfe0499bfdf3a20a3402e4
Bilwa Joglekar <bilwa,joglekar@gmail.com>
2018-12-06 15:37:38 -0500

    Final one

--- project/js/bucket.js
+++ project/js/bucket.js
@@ -70,37 +70,11 @@ function move() {
 
 
 
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
 
 
-function drawChart() {
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Skill');
-  data.addColumn('number', 'Points');
-  data.addRows([
-    ['Food', 200],
-    ['Lights', 500],
-    ['Furniture', 500],
-    ['Transport', 1450],
-    ['Tools', 500],
-
-]);
 
 
-// Set chart options
-var options = {
-  'title':'Funds',
-  'width':1000,
-  'height':500
-};
 
-
-// Display the chart
-if(window.location.pathname == "/Users/bilwa/Desktop/Eventus/project/charts.html"){
-  var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
-  chart.draw(data, options);
-}
-
-} // Don’t forget to close your function
+ $(".heart").on("click", function() {
+   $(this).toggleClass("is-active");
+ });
